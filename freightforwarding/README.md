cde156b2fbd803c7f94d448a91c5f3fd43b7e1fe
Fabian Blaicher <fabianblaicher@gmail.com>
2020-04-06 11:24:01 +0100

    Set up c# FF

--- freightforwarding/README.md
+++
@@ -1,5 +0,0 @@
-# Freight forwarding API
-
-This folder contains basic examples for testing the Shipamax freight forwarding API currently situated http://developer.shipamax-api.com/explorer/
-
-Please contact techsupport@shipamax.com with any inquiries.
---
+++ freightforwarding/csharp/ApiExample/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsoleApp1
+{
+    public class Credentials
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+
+    public class AccessDetails
+    {
+        public string id { get; set; }
+    }
+
+    public class DocumentContainers
+    {
+        public DocumentContainer[] results { get; set; }
+    }
+
+    public class DocumentContainer
+    {
+        public string unqId { get; set; }
+    }
+
+    public class CustomQueryParam
+    {
+        public string[] customIds { get; set; }
+        public string access_token { get; set; }
+    }
+	
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task<AccessDetails> LoginAsync(Credentials credentials)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/v1/users/login", credentials);
+            Console.WriteLine($"Username: {credentials.email}");
+            AccessDetails accessDetails = null;
+            if (response.IsSuccessStatusCode)
+            {
+                accessDetails = await response.Content.ReadAsAsync<AccessDetails>();
+            }
+            return accessDetails;
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://developer.shipamax-api.com/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                Credentials credentials = new Credentials
+                {
+                    email = "[YOUR-USERNAME]",
+                    password = "[YOUR-PASSWORD]"
+                };
+
+                AccessDetails accessDetails = await LoginAsync(credentials);
+                Console.WriteLine($"Logged in and received access token: {accessDetails.id}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
